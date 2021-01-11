08daebad28a6170b3d18a41f73a5b3f9974fb655
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-01-11 19:16:12 +0800

    json tag

--- name.go
+++ name.go
@@ -3,7 +3,7 @@ package sjqm
 import "liangzi.local/nongli/ganzhi"
 
 //六十甲子 0:甲子 1:乙丑 2:丙寅...59:癸亥
-var jz60 = ganzhi.MakeJZ60()
+var Jz60 = ganzhi.MakeJZ60()
 
 //九宫原始信息
 type JG struct {
@@ -25,16 +25,15 @@ type G struct {
 	ZHIFU   string `json:"zhifu"`    //值符
 	ZHISHI  string `json:"zhishi"`   //值使
 
-	G1   []string `json:"g_1"` //一宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G2   []string `json:"g_2"` //二宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G3   []string `json:"g_3"` //三宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G4   []string `json:"g_4"` //四宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G5   []string `json:"g_5"` //五宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G6   []string `json:"g_6"` //六宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G7   []string `json:"g_7"` //七宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G8   []string `json:"g_8"` //八宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	G9   []string `json:"g_9"` //九宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
-	JXXW *JXXW
+	G1 []string `json:"g_1"` //一宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G2 []string `json:"g_2"` //二宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G3 []string `json:"g_3"` //三宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G4 []string `json:"g_4"` //四宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G5 []string `json:"g_5"` //五宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G6 []string `json:"g_6"` //六宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G7 []string `json:"g_7"` //七宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G8 []string `json:"g_8"` //八宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
+	G9 []string `json:"g_9"` //九宫 九星 八门 暗干支 天盘奇仪 八神 地盘奇仪
 }
 
 //九宫排盘结果信息(map)
@@ -44,11 +43,12 @@ type GMap struct {
 
 //qm包结果信息(map)
 type QM struct {
-	DiSiHuMap     map[string]string //地四户
-	DiSiMenMap    map[string]string //地私门
-	TianMaS       string            //太冲天马
-	TianSanMenMap map[string]string //天三门
-	WuFuMap       map[string]string //五符
+	DiSiHu     string   `json:"di_si_hu"`     //地四户
+	DiSiMen    string   `json:"di_si_men"`    //地私门
+	TianMa     string   `json:"tian_ma"`      //太冲天马
+	TianSanMen string   `json:"tian_san_men"` //map[string]string //天三门
+	WuFu       []string `json:"wu_fus"`       //五符
+	GuXu       string   `json:"gu_xu"`        //时孤虚
 }
 
 //九星休旺
