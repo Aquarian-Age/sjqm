1c73932dd02c1255b7e62b95af3466bbafd5c9d9
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-05-01 10:05:53 +0800

    六十甲子

--- name.go
+++ name.go
@@ -1,9 +1,8 @@
 package sjqm
 
-import "liangzi.local/cal"
 
-//六十甲子 0:甲子 1:乙丑 2:丙寅...59:癸亥
-var Jz60 = cal.MakeJZ60()
+////六十甲子 0:甲子 1:乙丑 2:丙寅...59:癸亥
+var Jz60 = GetJZArr()
 
 //九宫原始信息
 type JG struct {
